0a37938d4fd108d3ace88fe34ff6ec3668b39479
Aniket04080218 <gupta.aniket0408@gmail.com>
2025-04-12 23:37:04 +0530

    latest

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,11 +7,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
   },
   server: {
     port: 5173,
