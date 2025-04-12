0e1d7c2207a587dcbbd1ff81ac2adc8f84604d94
Aniket04080218 <gupta.aniket0408@gmail.com>
2025-04-12 23:08:27 +0530

    update

--- server/server.js
+++ server/server.js
@@ -11,5 +11,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
