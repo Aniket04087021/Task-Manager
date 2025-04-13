760955f8e18f05435341146dd10dfec1d65770eb
Aniket04080218 <gupta.aniket0408@gmail.com>
2025-04-13 14:14:02 +0530

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
