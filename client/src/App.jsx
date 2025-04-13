da5670335bbc256f875a1f9f5d224400f7c1cfb3
Aniket04080218 <gupta.aniket0408@gmail.com>
2025-04-13 14:10:22 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -20,14 +18,11 @@ function App() {
   };
 
   return (
-    <ThemeProvider>
-      <ThemeToggle />
-      <Routes>
-        <Route path="/" element={token ? <Home logout={logout} /> : <Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage login={login} />} />
-        <Route path="/signup" element={<SignupPage login={login} />} />
-      </Routes>
-    </ThemeProvider>
+    <Routes>
+      <Route path="/" element={token ? <Home logout={logout} /> : <Navigate to="/login" />} />
+      <Route path="/login" element={<LoginPage login={login} />} />
+      <Route path="/signup" element={<SignupPage login={login} />} />
+    </Routes>
   );
 }
 
